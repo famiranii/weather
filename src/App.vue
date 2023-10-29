8aa039a2904c1e647765653a4c2f5c75efd6e1bc
farhad40 <you@example.com>
2023-10-29 16:03:14 +0330

    start

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import HeyD from "./components/HeyD.vue";
+const namee = ref<string>("f");
+</script>
+
+<template>
+
+  <h1>{{ namee }}</h1>
+  <HeyD msg="farga" />
+  <HelloWorld msg="Vite + Vue" title="fafe" />
+</template>
+
+<style scoped></style>
