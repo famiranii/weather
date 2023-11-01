cc49a0989031b6a6d28366e44f83caec4c86a3eb
farhad40 <you@example.com>
2023-11-01 10:37:37 +0330

    not found page is created

---
+++ src/pages/notFound/notFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>404 - Not Found</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+  </div>
+</template>
+
+<style scoped>
+
+div{
+    margin: auto 0;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,20 @@ import Dashboard from "../pages/dashboard/Dashboard.vue";
 import Todos from "../pages/todos/Todos.vue";
 import Weather from "../pages/weather/Weather.vue";
 import Profile from "../pages/profile/Profile.vue";
+import NotFound from "../pages/notFound/NotFound.vue";
 
 const routes = [
   { path: "/", component: Dashboard },
   { path: "/todos", component: Todos },
   { path: "/weather", component: Weather },
   { path: "/profile", component: Profile },
+  { path: "/:catchAll(.*)", component: NotFound },
 ];
 
 export default routes;
+
+
+
+
+
+
